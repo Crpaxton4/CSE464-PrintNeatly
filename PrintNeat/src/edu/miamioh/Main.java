11dfc412adfca729360251c0cfeb480ef3932063
Crpaxton4 <paxtoncr@miamioh.edu>
2018-01-24 21:40:56 -0500

    Total Cost Calculation

--- PrintNeat/src/edu/miamioh/Main.java
+++ PrintNeat/src/edu/miamioh/Main.java
@@ -2,143 +2,263 @@ package edu.miamioh;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * Consider the problem of neatly printing a paragraph with a monospaced font
  * (all characters having the same width) on a printer. The input text is a
- * sequence of n words of lengths l[0], l[1], ..., l[n-1] , measured in characters.
- * We want to print this paragraph neatly on a number of lines that hold a maximum
- * of M characters each. Our criterion of "neatness" is as follows. If a given
- * line contains words i through j, where i <= j , and we leave exactly one space
- * between words, the number of extra space characters at the end of the line is
- * [EDIT: you are to figure this out], which must be nonnegative so that the words
- * fit on the line. We wish to minimize the sum, over all lines except the last,
- * of the cubes of the numbers of extra space characters at the ends of lines.
- * Give a dynamic-programming algorithm to print a paragraph of n words neatly
- * on a printer. Analyze the running time and space requirements of your algorithm.
+ * sequence of n words of lengths l[0], l[1], ..., l[n-1] , measured in
+ * characters. We want to print this paragraph neatly on a number of lines that
+ * hold a maximum of M characters each. Our criterion of "neatness" is as
+ * follows. If a given line contains words i through j, where i <= j , and we
+ * leave exactly one space between words, the number of extra space characters
+ * at the end of the line is [EDIT: you are to figure this out], which must be
+ * nonnegative so that the words fit on the line. We wish to minimize the sum,
+ * over all lines except the last, of the cubes of the numbers of extra space
+ * characters at the ends of lines. Give a dynamic-programming algorithm to
+ * print a paragraph of n words neatly on a printer. Analyze the running time
+ * and space requirements of your algorithm.
  *
- * NOTE: I have provided two files (magna_carta.txt and kubla_kahn.txt). I know what
- * the correct cost is for each, and I know the approximate running
- * time (if you mess up DP then you will be off by several orders of magnitude).
+ * NOTE: I have provided two files (magna_carta.txt and kubla_kahn.txt). I know
+ * what the correct cost is for each, and I know the approximate running time
+ * (if you mess up DP then you will be off by several orders of magnitude).
  */
 public class Main {
 
-    static int INFINITY = Integer.MAX_VALUE;
-    static int MAXLINE = 80;
+	static int INFINITY = Integer.MAX_VALUE;
+	static int MAXLINE = 80;
 
-    static String[] readWordsFromFile(String path) throws IOException {
-        Scanner scanner = new Scanner(new File(path));
-        ArrayList<String> words = new ArrayList<String>();
+	final static class Line {
+		final int i;
+		final int j;
+		final int cost;
 
-        while (scanner.hasNext())
-            words.add(scanner.next());
+		public Line(int i, int j, int cost) {
+			this.i = i;
+			this.j = j;
+			this.cost = cost;
+		}
+	}
 
-        return words.toArray(new String[words.size()]);
-    }
+	static String[] readWordsFromFile(String path) throws IOException {
+		Scanner scanner = new Scanner(new File(path));
+		ArrayList<String> words = new ArrayList<String>();
 
+		while (scanner.hasNext())
+			words.add(scanner.next());
 
-    static int unusedSpace(int i, int j, int[] cumSumOfWordLengths, int maxline){
-    	//If S[k] is the cumulative sum of word lengths up to word k, or 0 if k < 0
-        //Then this should be the value M - (S[j] - S[i-1] + j - i)
-        return maxline - (cumSumOfWordLengths[j] - cumSumOfWordLengths[i-1] + j - i); 
-    }
+		scanner.close();
+		return words.toArray(new String[words.size()]);
+	}
 
-    static int printNeatly(String[] words, int maxline, OutputStream out){
-        // Minimize the cost of printing the words.
-        // The cost is the amount of unused space on each line (cubed),
-        // or INFINITY of the words don't fit.
-        // The last line has no cost.
-        // (e.g. there is no penalty for unused spaces on the last line).
+	static int lineCost(int i, int j, int[] cumSumOfWordLengths, int maxline) {
+		// If S[k] is the cumulative sum of word lengths up to word k, or 0 if k < 0
+		// Then this should be the value M - (S[j] - S[i-1] + j - i)
+		if (i > j) {
+			return INFINITY;
+		}
 
+		int extraSpaces = maxline - (cumSumOfWordLengths[j] - cumSumOfWordLengths[i - 1] + j - i);
 
-        return INFINITY; //The total cost (sum of unusedSpace^3 for all but last line)
-    }
+		if (extraSpaces < 0) {
+			return INFINITY;
+		} else if (j == cumSumOfWordLengths.length - 1 && extraSpaces >= 0) {
+			return 0;
+		} else {
+			return (int) Math.pow(extraSpaces, 3);
+		}
+	}
 
-    static boolean checkThatNoLinesStartOrEndWithSpaces(String text){
-    	String[] lines = text.split("\n");
-    	for(String line : lines) {
-    		if(!line.equals(line.trim())) {
-    			return false;
-    		}
-    	}
-        return true; // false means extra spaces found
-    }
+	static int printNeatly(String[] words, int maxLine, OutputStream out) throws IOException {
+		// Minimize the cost of printing the words.
+		// The cost is the amount of unused space on each line (cubed),
+		// or INFINITY of the words don't fit.
+		// The last line has no cost.
+		// (e.g. there is no penalty for unused spaces on the last line).
+		Stack<Line> solution = dynamicSolution(words, maxLine);
+		StringBuffer str = new StringBuffer();
+		Line l = null;
 
-    static boolean checkThatNoMoreThanOneSpaceBetweenWords(String text){
-    	String[] words = text.split(" ");
-    	for(String word : words) {
-    		if(word.isEmpty()) {
-    			return false;
-    		}
-    	}
-        
-        return true; //false means multiple spaces in a row were found
-    }
+		while (!solution.empty()) {
+			l = solution.pop();
+			str.append(getLineAsStringBuffer(words, l));
+			str.append("\n");
+		}
 
-    static boolean checkThatAllLinesAreLessThanMax(String text, int maxline){
-    	String[] lines = text.split("\n");
-    	for(String line : lines) {
-    		if(line.length() > maxline) {
-    			return false;
-    		}
-    	}
-        return true; // false means you output a line that was too long
-    }
+		out.flush();
+		out.write(str.toString().getBytes());
 
-	static int computeCostFromOutput(String text, int maxline){
-    	String[] lines = text.split("\n");
-    	int totalCost = 0;
-    	
-    	for(String line : lines) {
-    		totalCost += Math.pow((maxline - line.length()), 3);
-    	}
-    
-        return totalCost;  // Compute the cost to verify that
-                          // it matches what we get from printNeatly
-    }
+		return l.cost; // The total cost (sum of unusedSpace^3 for
+						// all but last line)
+	}
 
-    public static void main(String[] args) {
+	public static StringBuffer getLineAsStringBuffer(String[] words, Line l) {
+		StringBuffer str = new StringBuffer();
+		int i = l.i;
+		int j = l.j;
 
-        for (String arg:args) {
-            try {
-                String[] words = readWordsFromFile(arg);
+		str.append(words[i - 1]);
+		for (int k = i; k < j - 1; k++) {
+			str.append(" ");
+			str.append(words[k]);
 
+		}
 
-                ByteArrayOutputStream result = new ByteArrayOutputStream();
-                long trials = 0;
-                long total = 0;
-                long stop = 0;
-                long start = System.currentTimeMillis();
-                long cost = 0;
-                while (total < 30*1000){
-                    result.reset();
-                    cost = printNeatly(words, MAXLINE,  result);
-                    stop = System.currentTimeMillis();
-                    total = stop-start;
-                    trials += 1;
-                }
-                float average = total / (float)trials;
+		return str;
+	}
 
-                String text = result.toString();
-                assert checkThatAllLinesAreLessThanMax(text, MAXLINE);
-                assert checkThatNoLinesStartOrEndWithSpaces(text);
-                assert checkThatNoMoreThanOneSpaceBetweenWords(text);
-                assert computeCostFromOutput(text, MAXLINE) == cost;
+	static Stack<Line> dynamicSolution(String[] words, int maxLine) {
 
-                System.out.print(arg);
-                System.out.print(", average running time: ");
-                System.out.println(average);
-                System.out.print(", cost: ");
-                System.out.println(cost);
+		int[] cumSumOfWordLengths = cumSumOfWordLengths(words);
+		int[] solutions = new int[cumSumOfWordLengths.length];
+		int[] startOflines = new int[cumSumOfWordLengths.length];
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+		solutions[0] = 0;
 
-        }
-    }
+		for (int j = 1; j < cumSumOfWordLengths.length; j++) {
+
+			int min = INFINITY;
+			int index = -1;
+			for (int i = 1; i <= j; i++) {
+
+				int totalCost;
+				if (lineCost(i, j, cumSumOfWordLengths, maxLine) == INFINITY) {
+					totalCost = INFINITY;
+				} else {
+					totalCost = solutions[i - 1] + lineCost(i, j, cumSumOfWordLengths, maxLine);
+				}
+
+				if (totalCost < min) {
+					min = totalCost;
+					index = i;
+				}
+
+			}
+			System.out.println("Total Cost [" + j + "] = " + min + " : [" + index + ", " + j + "]");
+			solutions[j] = min;
+			startOflines[j] = index;
+
+		}
+
+		Stack<Line> lines = new Stack<Line>();
+
+		int end = words.length;
+
+		
+		do {
+			Line l = new Line(startOflines[end], end, solutions[end]);
+			lines.push(l);
+			end = startOflines[end] - 1;
+		}while (end != startOflines[1] - 1);
+
+		return lines;
+	}
+
+	static int[] cumSumOfWordLengths(String[] words) {
+		int[] cumSumOfWordLengths = new int[words.length + 1];
+		cumSumOfWordLengths[0] = 0;
+
+		for (int i = 1; i <= words.length; i++) {
+			cumSumOfWordLengths[i] = cumSumOfWordLengths[i - 1] + words[i - 1].length();
+			System.out.println(cumSumOfWordLengths[i] + " " + words[i - 1]);
+		}
+
+		return cumSumOfWordLengths;
+
+	}
+
+	static boolean checkThatNoLinesStartOrEndWithSpaces(String text) {
+		String[] lines = text.split("\n");
+		for (String line : lines) {
+			if (!line.equals(line.trim())) {
+				return false;
+			}
+		}
+		return true; // false means extra spaces found
+	}
+
+	static boolean checkThatNoMoreThanOneSpaceBetweenWords(String text) {
+		String[] words = text.split(" ");
+		for (String word : words) {
+			if (word.isEmpty()) {
+				return false;
+			}
+		}
+
+		return true; // false means multiple spaces in a row were found
+	}
+
+	static boolean checkThatAllLinesAreLessThanMax(String text, int maxline) {
+		String[] lines = text.split("\n");
+		for (String line : lines) {
+			if (line.length() > maxline) {
+				return false;
+			}
+		}
+		return true; // false means you output a line that was too long
+	}
+
+	static int computeCostFromOutput(String text, int maxline) {
+		String[] lines = text.split("\n");
+		int totalCost = 0;
+
+		for (String line : lines) {
+			totalCost += Math.pow((maxline - line.length()), 3);
+		}
+
+		return totalCost; // Compute the cost to verify that
+							// it matches what we get from printNeatly
+	}
+
+	public static void main(String[] args) {
+
+		for (String arg : args) {
+			try {
+				String[] words = readWordsFromFile(arg);
+				System.out.println(Arrays.toString(words));
+
+				ByteArrayOutputStream result = new ByteArrayOutputStream();
+				long trials = 0;
+				long total = 0;
+				long stop = 0;
+				long start = System.currentTimeMillis();
+				long cost = 0;
+				// while (total < 1) {
+				result.reset();
+				cost = printNeatly(words, MAXLINE, result);
+				stop = System.currentTimeMillis();
+				total = stop - start;
+				trials += 1;
+				// }
+				float average = total / (float) trials;
+
+				String text = result.toString();
+				assert checkThatAllLinesAreLessThanMax(text, MAXLINE);
+				assert checkThatNoLinesStartOrEndWithSpaces(text);
+				assert checkThatNoMoreThanOneSpaceBetweenWords(text);
+				assert computeCostFromOutput(text, MAXLINE) == cost;
+
+				FileWriter f = new FileWriter("output.txt");
+				f.write(text);
+				f.close();
+
+				System.out.print(arg);
+				System.out.print(", average running time: ");
+				System.out.println(average);
+				System.out.print(", cost: ");
+				System.out.println(cost);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
 }
